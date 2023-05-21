5b546c17190a288e676183ea44870e0ee7dd78aa
Ric Charles Paquibot <ric2thiss@gmail.com>
2023-05-22 00:50:38 +0800

    first commit

---
+++ src/components/brand/import.js
@@ -0,0 +1,13 @@
+import google from '../../assests/google.png'
+import shopify from '../../assests/shopify.png'
+import slack from '../../assests/slack.png'
+import dropbox from '../../assests/dropbox.png'
+import atlassian from '../../assests/atlassian.png'
+
+export {
+    google,
+    shopify,
+    atlassian,
+    slack,
+    dropbox,
+}
